3b4c5256dcf74832a018c1cb6b347a7935186eec
Max <gievanom@lbbstudios.net>
2020-11-13 00:53:19 +0100

    Take Two: heroku's tutorial

--- index.js
+++ index.js
@@ -2,17 +2,31 @@
 //Version: 1.0
 //License (LGPL-2.1) + repo: https://github.com/LBBStudios/RexSyncServer
 
-const http = require('http');
-const WebSocketServer = require('websocket').server;
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
 
-const server = http.createServer();
-server.listen(process.env.PORT || 5000);
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const wsServer = new WebSocketServer({
-    httpServer: server
+const { Server } = require('ws');
+
+const wss = new Server({ server });
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+    ws.on('close', () => console.log('Client disconnected'));
 });
 
-wsServer.on('request', function(request) {
+//This sends something without the client asking for it
+//Will re-tool this later for the currentTime value / play state.
+setInterval(() => {
+    wss.clients.forEach((client) => {
+        client.send(new Date().toTimeString());
+    });
+}, 1000);
+
+/*wsServer.on('request', function(request) {
     const connection = request.accept(null, request.origin);
 
     connection.on('message', function(message) {
@@ -23,4 +37,4 @@ wsServer.on('request', function(request) {
     connection.on('close', function(reasonCode, desc) {
         console.log("Client has disconnected for reason: " + reasonCode + " (" + desc + ")");
     });
-});
+});*/
