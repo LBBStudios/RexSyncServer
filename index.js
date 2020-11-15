f9eccbb41d31909ea79ce7872d25a00f8881a460
Max <gievanom@lbbstudios.net>
2020-11-15 16:01:59 +0100

    Better userID generation

--- index.js
+++ index.js
@@ -29,8 +29,18 @@ var users = {};
 //================================================== UTILITY FUNCTIONS ==================================================
 
 //Function to generate a new userID
+//Taken from: https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
 function generateUserID() {
-  return "User" + Math.random();
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  var length = 16;
+
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return "User" + result;
 }
 
 //================================================== WEB HEALTH CHECKS ==================================================
