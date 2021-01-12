4f56a586d542601b12d6e6df9509f14295cb12ea
Max <gievanom@lbbstudios.net>
2021-01-13 00:18:13 +0100

    Server: handle client messages, room creation

--- index.js
+++ index.js
@@ -23,15 +23,15 @@ app.use(function(req, res, next) {
 });
 
 //To keep track of users/rooms:
-var rooms = {};
-var users = {};
-var version = "RexSyncServer - v0.4";
+var rooms = [];
+var users = [];
+var version = "RexSyncServer - v0.5";
 
 //================================================== UTILITY FUNCTIONS ==================================================
 
 //Function to generate a new userID
 //Taken from: https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
-function generateUserID() {
+function generateRandomCharacters() {
   var result           = '';
   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   var charactersLength = characters.length;
@@ -41,7 +41,15 @@ function generateUserID() {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
 
-  return "User" + result;
+  return result;
+}
+
+function generateUserID() {
+  return "User_" + generateRandomCharacters();
+}
+
+function generateRoomID() {
+  return "Room_" + generateRandomCharacters();
 }
 
 //================================================== WEB HEALTH CHECKS ==================================================
@@ -81,10 +89,57 @@ io.on('connection', function(socket) {
   }
 
   //If we get here, then they have their ID:
+  users.push({
+    id: userID,
+    socket: socket,
+    roomID: ""
+  });
+
   socket.emit('userID', userID); //send it to the client
   console.log("User " + userID + " has connected to RexSync.");
 
-  //TODO: Handle message from the client here and send appropriate responses.
+  //Handle messages from the client here and send appropriate responses:
+
+  socket.on('play', (timeString, roomID) => {
+    for (let val of users) {
+      if (val.roomID == roomID) {
+        val.socket.emit('play', timeString);
+      }
+    }
+  });
+
+  socket.on('pause', (timeString, roomID) => {
+    for (let val of users) {
+      if (val.roomID == roomID) {
+        val.socket.emit('pause', timeString);
+      }
+    }
+  });
+
+  socket.on('joinRoom', (roomID, userID) => {
+    console.log("user " + userID + " is trying to join room " + roomID);
+
+    for (let val of users) {
+      if (val.id == userID) {
+        if (!rooms.hasOwnProperty(roomID)) {
+          rooms[roomID] = {};
+          console.log("creating new room on server side: " + roomID);
+        }
+
+        val.roomID = roomID;
+        val.socket.emit('joinedRoom', roomID);
+      }
+    }
+  });
+
+  socket.on('seek', (timeString, roomID) => {
+    for (let val of users) {
+      if (val.roomID == roomID) {
+        val.socket.emit('seek', timeString);
+      }
+    }
+  });
+
 });
 
 //Actually start the server:
